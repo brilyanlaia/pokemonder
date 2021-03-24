fcf0596505bf63db81712ecb38892f6076609d41
unknown <brilyanlaia@gmail.com>
2021-03-24 23:58:15 +0700

    removed unused

--- src/Components/MyPokemon.js
+++ src/Components/MyPokemon.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import ModalComponent from "./ModalComponent";
 import { useHistory } from "react-router-dom";
 
